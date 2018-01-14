db52bb035c33d7855e2ddaee667dee30e17bee64
Snel <snelson10@sfccmo.edu>
2018-01-14 22:19:06 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {Queen: "We are the champion", Orangerange: "Hana", Pornographity: "Saboten"};
+
+
+
+
+
+
+
+
+
+function updatePlaylist ({}, playlist, {artistName: songTitle}){
+  Object.assign (artistName, songTitle)
+  return 
+}
